2fc5797866b8b66144bd5bfe8ce439969802f123
DasHeinzchen <marlon.heinz@outlook.de>
2021-07-26 14:52:09 +0200

    removed unused imports

--- src/main/java/MoreOres/block/Crusher.java
+++ src/main/java/MoreOres/block/Crusher.java
@@ -1,10 +1,5 @@
 package MoreOres.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.state.StateContainer;
-
 public class Crusher extends MachineBlock
 {
     public Crusher(Properties properties)
